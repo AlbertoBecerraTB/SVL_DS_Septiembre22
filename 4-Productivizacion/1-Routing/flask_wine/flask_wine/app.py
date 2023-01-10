5d92806ad3fa168b42abe8c680a85c62ed02ca61
Alberto Becerra <alberto.becerra@thebridgeschool.es>
2023-01-10 14:36:50 +0100

    APIs 2

--- 4-Productivizacion/1-Routing/flask_wine/flask_wine/app.py
+++ 4-Productivizacion/1-Routing/flask_wine/flask_wine/app.py
@@ -1,25 +1,27 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, jsonify
 import numpy as np
-import pickle as p
+import pickle as pkl
 import json
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/health/', methods=['GET'])
+modelfile = '4-Productivizacion\\1-Routing\\flask_wine\\flask_wine\\models\\final_prediction.pickle'
+model = pkl.load(open(modelfile, 'rb'))
+d_wines = {0: "tinto", 1: "blanco", 2: "rosado"}
+
+@app.route('/health', methods=['GET'])
 def health():
-    return "everything os here"
+    return "everything OK here"
 
-@app.route('/api/', methods=['POST'])
+@app.route('/score', methods=['POST'])
 def makecalc():
-    data = request.get_json()
-    prediction = np.array2string(model.predict(data))
-    return jsonify(prediction)
-
-modelfile = './1-Routing/flask_wine/flask_wine/models/final_prediction.pickle'
-model = p.load(open(modelfile, 'rb'))
-
-print(type(model))
+    data = request.get_json().values()
+    data = np.array(list(data)).reshape(1, -1)
+    prediction = int(model.predict(data)[0])
+    prediction = d_wines[prediction]
+    return f'<h1>¿Cuál es mi vino favorito?</h1><p>Mi Vino favorito es el {prediction}</p>'
 
 if __name__ == "__main__":
     print("hello")
